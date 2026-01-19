bea419268039d54e0b49b82a199775f01990438e
Srivaxshana <srivaxshana1604@gmail.com>
2026-01-19 11:38:56 +0530

    flutter setup and  structure

---
+++ android/app/src/main/kotlin/com/campus/events/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.campus.events
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/build.gradle.kts
@@ -0,0 +1,32 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = file("../build")
+
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
